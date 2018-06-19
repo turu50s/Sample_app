3c4642584e036182884c63b261ca1094107ee4e2
Shigeru Tsuruki <sigeru0216@gmail.com>
2018-06-20 08:22:12 +0900

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
     # @user = User.new(params[:user])
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App"
       redirect_to @user
       # redirect_to user_url(@user)
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,24 @@
 Rails.application.routes.draw do
+  # get 'sessions/new'  <- rails generate controllerで生成された
+
   # root 'application#hello'
   root 'static_pages#home'
   # get 'static_pages/home'
 
   # get 'static_pages/help'
-  get '/help', to: 'static_pages#help'
+  get  '/help', to: 'static_pages#help'
   # get 'static_pages/about'
-  get '/about', to: 'static_pages#about'
+  get  '/about', to: 'static_pages#about'
   # get 'static_pages/contact'
-  get '/contact', to: 'static_pages#contact'
+  get  '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/signup', to:'users#new'
+  get  '/signup', to:'users#new'
   post '/signup', to: 'users#create'
+
+  # Sessions Resources
+  get  '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
 end
