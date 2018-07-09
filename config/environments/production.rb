25a76f02228a950ae88bad461c47aebe88c36f6e
Shigeru Tsuruki <sigeru0216@gmail.com>
2018-07-09 11:59:46 +0900

    production.rb rewrite

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port    => '587',
-    :authentication => :plan,
+    :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
     :password  => ENV['SENDGRID_PASSWORD'],
     :domain    => 'heroku.com',
