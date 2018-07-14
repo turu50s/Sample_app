4ea437936baae8fee808ec52fd05af8dd59c6841
Shigeru Tsuruki <sigeru0216@gmail.com>
2018-07-14 16:15:05 +0900

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,6 +65,24 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now  #@userがselfに変更
   end
 
+  # パスワード再設定用の属性を設定する
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: User.digest(reset_token),
+                  reset_sent_at: Time.zone.now)
+    # update_attribute(:reset_digest, User.digest(reset_token))
+    # update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # パスワード再設定のメールを送信する
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
     # メールアドレスをすべて小文字にする
     def downcase_email
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
+  # get 'password_resets/new'
 
-  get 'password_resets/edit'
+  # get 'password_resets/edit'
 
   # get 'sessions/new'  <- rails generate controllerで生成された
 
